8cba34b0282b802b0639a0675c0980f971b228c4
tranhuy105 <imhuy105@gmail.com>
2024-04-09 20:25:17 +0700

    add docker compose

--- src/main/java/com/example/jdbcexample/JdbcExampleApplication.java
+++ src/main/java/com/example/jdbcexample/JdbcExampleApplication.java
@@ -1,12 +1,7 @@
 package com.example.jdbcexample;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class JdbcExampleApplication {
@@ -34,5 +29,6 @@ public class JdbcExampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(JdbcExampleApplication.class, args);
     }
+
 
 }
